bbb169a7ed835e1c193e07ea9ba74d5bd7913a8d
Mohd Asan <104296229+ofcyouritachii@users.noreply.github.com>
2026-02-10 05:03:30 +0000

    Refactor main.py to remove Flask integration and simplify chat functionality; delete index.html template

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for
 from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import PromptTemplate
 
-app = Flask(__name__)
+model = OllamaLLM(
+    model="tinyllama",
+    base_url="http://localhost:11434"
+)
 
-model = OllamaLLM(model="tinyllama", base_url="http://localhost:11434")
+template = """
+You are a helpful tutor.
+Explain {topic} in simple Hinglish.
+"""
 
-messages = []
+prompt = PromptTemplate(
+    input_variables=["topic"],
+    template=template
+)
 
-@app.route('/')
-def index():
-    return render_template('index.html', messages=messages)
+chain = prompt | model
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    user_message = request.form.get('message')
-    if user_message:
-        messages.append({'user': 'You', 'text': user_message})
-        prompt = f"You are a helpful tutor. Explain {user_message} in English."
-        response = model.invoke(prompt)
-        messages.append({'user': 'Bot', 'text': response})
-    return redirect(url_for('index'))
-
-@app.route('/clear', methods=['POST'])
-def clear():
-    messages.clear()
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+response = chain.invoke({"topic": "API"})
+print(response)
